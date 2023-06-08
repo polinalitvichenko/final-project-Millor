ecfff55d6a49d43765db6e7dc1f45a1f04a277b4
polinalitvichenko <polinalitvichenko@gmail.com>
2023-06-08 16:06:20 +0600

    [initial]

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import './globals.css'
+import { Nunito } from 'next/font/google'
+
+const nunito = Nunito({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'кофе +',
+  description: 'Всё о кофе',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={nunito.className}>
+
+      {children}
+      </body>
+    </html>
+  )
+}
